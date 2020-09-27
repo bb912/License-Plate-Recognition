84568c58fcb908e566a098e5afb45a7bdfa8867a
root <root@instance-2.us-central1-a.c.festive-radar-290704.internal>
2020-09-27 03:10:35 +0000

    back to local

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,8 +1,5 @@
 from api import api as application
 
-activate_this = '/home/brettbissey/anaconda3/envs/flask/bin/activate.py'
+activate_this = '/home/brettbissey/anaconda3/envs/flask/bin/activate'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
-
-
-
---
+++ flask/app.py
@@ -0,0 +1,263 @@
+
+from flask import Flask, render_template, request, redirect, url_for, make_response
+import hashlib
+import pymysql
+from sqlalchemy import create_engine, or_, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Customer, Service
+from gevent.pywsgi import WSGIServer
+from werkzeug.utils import secure_filename
+from datetime import datetime
+
+app = Flask(__name__)
+
+# THIS IS WHERE THE UPLOADED FILES GET SAVED
+app.config['UPLOAD_FOLDER'] = '/tmp/cars'
+engine = create_engine('mysql+mysqlconnector://lp:plate@35.237.243.227/auto')
+Base.metadata.create_all(engine)
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+@app.errorhandler(404)
+def not_found():
+		return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+
+"""
+
+Customers API
+@app.route('/CustomersApi', methods=['POST'])
+		-add new customers for database input
+@app.route('/CustomersApi/<int:id>', methods=['GET', 'POST'])
+		-update or get customer based on id
+@app.route('/CustomersApi/<int:id>/delete', methods=['POST'])
+		-delete customer based on id
+
+
+"""
+from flask import jsonify
+
+
+# get a single Customer by its id number
+def get_customer(lp_num):
+		customer = session.query(Customer).filter_by(LicensePlate = lp_num).one()
+		return jsonify(Customer=Customer.serialize)
+
+# create a new Customer given all information
+def create_customer(user_id, first_name, last_name, phone, email, lp_num):
+		add_customer = Customer(FirstName=first_name, LastName=last_name,
+														PhoneNumber=phone,
+														Email=email,
+														LicensePlate=lp_num)
+		session.add(added_Customer)
+		session.commit()
+		return "Added Customer with id %s" % added_Customer.ID
+
+# delete Customer by Customer ID
+def delete_customer(id):
+		Customer_to_delete = session.query(Customer).filter_by(ID=id).one()
+		session.delete(Customer_to_delete)
+		session.commit()
+		return "Removed Customer with id %s" % id
+
+
+# update an existing Customer
+def update_customer(customer_id, first_name, last_name, phone, email, lp_num):
+		updated_Customer = session.query(Customer).filter_by(ID=customer_id).one()
+
+		if first_name:
+				updated_Customer.FirstName = first_name
+		if last_name:
+				updated_Customer.LastName = last_name
+		if phone:
+				updated_Customer.PhoneNumber = phone
+		if email:
+				updated_Customer.Email = email
+		if lp_num:
+				updated_Customer.LicensePlate = lp_num
+
+		session.add(updated_Customer)
+		session.commit()
+
+		return "Updated Customer with id %s" % Customer_id
+
+# list Customers or add a Customer (for a specific user)
+
+# GET request requires user_id in json
+
+#POST request requires first_name, last_name, phone, email, user_id,
+
+#ADDING A Customer FOR A USER
+@app.route('/')
+@app.route('/CustomersApi', methods=['POST'])
+def PostNewCustomer():
+
+		#if request.method == "OPTIONS": # CORS preflight
+        #		return _build_cors_prelight_response()
+
+		body = request.get_json(force=True)
+
+		first = body.get('FirstName', '')
+		last = body.get('LastName', '')
+		phone = body.get('PhoneNumber', '')
+		email = body.get('Email', '')
+		user = body.get('UserID', '')
+		return create_new_Customer(user, first, last, phone, email)
+
+
+# get a specific Customer by Customer ID, or update Customer, or delete Customer
+@app.route('/CustomersApi/<int:id>', methods=['GET', 'POST'])
+#@cross_origin()
+def CustomersFunctionID(id):
+		if request.method == 'GET':
+				return get_Customer(id)
+
+		elif request.method == 'POST':
+
+				body = request.get_json(force=True)
+
+				first = body.get('FirstName', '')
+				last = body.get('LastName', '')
+				phone = body.get('PhoneNumber', '')
+				email = body.get('Email', '')
+				user = body.get('UserID', '')
+				return update_Customer(id, first, last, phone, email)
+
+
+# get a specific Customer by Customer ID, or update Customer, or delete Customer
+@app.route('/CustomersApi/<int:id>/delete', methods=['POST'])
+#@cross_origin()
+def CustomersFunctionDelete(id):
+		return delete_Customer(id)
+
+
+
+"""
+
+Service API
+
+
+
+"""
+
+# get a single Customer by its id number
+def get_sevices(lp_num):
+		service = session.query(Customer).filter_by(LicensePlate = lp_num).one()
+		return jsonify(Customer=Customer.serialize)
+
+# create a new service given all information
+def create_service(customer_id, service, advisor_name, date):
+		add_service = service(CustomerID=customer_id, Service=service,
+														AdvisorName=advisor_name,
+														Date=date)
+		session.add(added_service)
+		session.commit()
+		return "Added service with id %s" % added_service.ID
+
+# delete service by service ID
+def delete_service(id):
+		service_to_delete = session.query(service).filter_by(ID=id).one()
+		session.delete(service_to_delete)
+		session.commit()
+		return "Removed service with id %s" % id
+
+
+# update an existing service
+def update_service(service_id, customer_id, service, advisor_name, date):
+
+		if not service_id:
+			return "Error, include sevice_id"
+
+		updated_service = session.query(service).filter_by(ID=service_id).one()
+
+		if customer_id:
+				updated_service.CustomerID = customer_id
+		if service:
+				updated_service.Service = service
+		if advisor_name:
+				updated_service.AdvisorName = advisor_name
+		if date:
+				updated_service.Date = date
+
+
+		session.add(updated_service)
+		session.commit()
+
+		return "Updated service with id %s" % service_id
+
+
+
+# ADDING A service FOR A USER
+@app.route('/')
+@app.route('/servicesApi', methods=['POST'])
+def PostNewservice():
+
+		body = request.get_json(force=True)
+
+		cid = body.get('CustomerID', '')
+		service = body.get('Service', '')
+		advisor = body.get('AdvisorName', '')
+		date = body.get('Date', '')
+		return create_new_service(sid, cid, service, advisor, date)
+
+
+# get a specific service by service ID, or update service, or delete service
+@app.route('/servicesApi/<int:id>', methods=['GET', 'POST'])
+def servicesFunctionID(id):
+		if request.method == 'GET':
+				return get_service(id)
+
+		elif request.method == 'POST':
+
+				body = request.get_json(force=True)
+
+				first = body.get('FirstName', '')
+				last = body.get('LastName', '')
+				phone = body.get('PhoneNumber', '')
+				email = body.get('Email', '')
+				user = body.get('UserID', '')
+				return update_service(id, first, last, phone, email)
+
+
+# get a specific service by service ID, or update service, or delete service
+@app.route('/servicesApi/<int:id>/delete', methods=['POST'])
+#@cross_origin()
+def servicesFunctionDelete(id):
+		return delete_service(id)
+
+
+
+
+"""
+Photo uploading endpoints and user searching
+
+"""
+
+# for updating User's personal information
+@app.route('/whichCustomer', methods=['POST'])
+#@cross_origin()
+def whichCustomer():
+		if request.method == 'POST':
+				file1 = request.files.get('a', None);
+				file2 = request.files.get('b', None);
+				file3 = request.files.get('c', None);
+
+				# TODO: CALL THE BASH SCRIPT HERE ... conda activate && etc
+
+				# scrape csv file
+
+				# "vote" on the result, call above functions to search for which customer it is
+
+				return None
+
+
+if __name__ == '__main__':
+		pymysql.install_as_MySQLdb()
+		app.debug = True
+		http_server = WSGIServer(('', 4996), app)
+
+		http_server.serve_forever()
+		#app.run(host='0.0.0.0', port=4996)
