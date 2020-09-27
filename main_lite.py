bee46024b705fdca63f7e3edd00304de32f1bb10
root <root@instance-2.us-central1-a.c.festive-radar-290704.internal>
2020-09-27 06:01:31 +0000

    update from VM

--- main_lite.py
+++ main_lite.py
@@ -40,7 +40,7 @@ def gen(camera):
         #get camera frame
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n'))
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 
